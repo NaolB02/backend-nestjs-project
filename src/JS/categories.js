d6c51ba317120ac013ed04a3a8e5e641f434017f
Mihretab Nigussie <110567717+MihretabNigussie@users.noreply.github.com>
2023-02-04 09:46:17 +0300

    Add files via upload

---
+++ src/JS/categories.js
@@ -0,0 +1,15 @@
+const likeBtn = document.querySelectorAll('.main__container__likebtn');
+const dislikeBtn = document.querySelectorAll('.dislikebtn');
+
+for (let i = 0; i < likeBtn.length; i++) {
+    likeBtn[i].addEventListener('click', function(){
+        
+    })
+}
+
+for (let i = 0; i < dislikeBtn.length; i++){
+    dislikeBtn = addEventListener('click', function(){
+        alert('Kidist hager Ethiopia');
+    })
+}
+
